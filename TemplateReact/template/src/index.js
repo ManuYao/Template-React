8b815b48d672b7f355e734a4360876397f73d9f1
Manu Y <mine.boy230@gmail.com>
2022-12-23 02:25:36 +0100

    Alpha 0.6.1
    ADD : Commantaire  : index.js

--- TemplateReact/template/src/index.js
+++ TemplateReact/template/src/index.js
@@ -6,12 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>  strict n'est pas destiné à être utilisé en production, car il peut avoir un impact sur les performances de votre application.
     <App />
-  </React.StrictMode>
+  //</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
